ebafc9110285501b531571b423ac34a8a7cd9615
Andrii Kulyhin <evilandruyha@gmail.com>
2023-04-14 19:29:16 +0300

    Low poly buildings & prefabs & buildings script

---
+++ Assets/Scripts/Builder/Building.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Builder
+{
+    public class Building : MonoBehaviour
+    {
+        [SerializeField] private Vector2Int gridSize = Vector2Int.one;
+
+        private void OnDrawGizmosSelected()
+        {
+            for (var x = 0; x < gridSize.x; x++)
+            {
+                for (var y = 0; y < gridSize.y; y++)
+                {
+                    Gizmos.color = (x + y) % 2 == 0 ? new Color(1f, 0f, 1f) : Color.black;
+                    Gizmos.DrawCube(transform.position + new Vector3(x, 0, y), new Vector3(1, 0.1f, 1));
+                }
+            }
+        }
+    }
+}
